76c8a862e55e375349d07c61f821506872cbcfd5
Valthecodergit config --global user.email keshia.kdf@gmail.com <keshia.kdf@gmail.com>
2024-01-08 17:58:31 +0800

    Ask 3 Numbers

--- BiggestNumberFinder.py
+++ BiggestNumberFinder.py
@@ -1,4 +1,6 @@
-# Setting Variables and their Value
 # Ask user for three inputs. Check if vowel or consonant
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+num3 = float(input("Enter the 3rd number: "))
 # Creating if functions
 # Design for wow factor
