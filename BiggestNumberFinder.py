a1efaf09f130cbcebe8930970eddfa7a261f1650
Valthecodergit config --global user.email keshia.kdf@gmail.com <keshia.kdf@gmail.com>
2024-01-08 18:09:41 +0800

    Find the Biggest Number

--- BiggestNumberFinder.py
+++ BiggestNumberFinder.py
@@ -3,4 +3,11 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 num3 = float(input("Enter the 3rd number: "))
 # Creating if functions
+if num2 <= num1 and num3 <= num1:
+    print ("The Biggest Number is: Num 1")
+elif num1 <= num2 and num3 <= num2:
+    print ("The Biggest Number is: Num 2")
+else:
+    print ("The Biggest Number is: Num 3")
+
 # Design for wow factor
