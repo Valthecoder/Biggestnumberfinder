a3eb8b95a09ffa8279826b821e6bce7f6b1cd0d0
Valthecodergit config --global user.email keshia.kdf@gmail.com <keshia.kdf@gmail.com>
2024-01-08 20:05:53 +0800

    Pseudo Code for Tkinter

--- BiggestNumberFinder.py
+++ BiggestNumberFinder.py
@@ -2,16 +2,18 @@
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 num3 = float(input("Enter the 3rd number: "))
-# Creating if functions
+
 if num2 < num1 and num3 < num1:
-    print ("The Biggest Number is: Num 1")
+    print("The Biggest Number is: Num 1")
 elif num1 < num2 and num3 < num2:
-    print ("The Biggest Number is: Num 2")
+    print("The Biggest Number is: Num 2")
 elif num1 == num2 == num3:
-    print ("All Numbers are Equal")
+    print("All Numbers are Equal")
 else:
-    print ("The Biggest Number is: Num 3")
+    print("The Biggest Number is: Num 3")
 
 # Design for wow factor
-
-
+# Display a new window using message box
+# Change some variables and functions since new window will be used
+# Input Labels
+# Create Button for trigger
