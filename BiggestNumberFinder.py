c7fd3e551194d16310c338ff5ebe640a1096143a
Valthecodergit config --global user.email keshia.kdf@gmail.com <keshia.kdf@gmail.com>
2024-01-08 18:21:38 +0800

    Correction for Equal

--- BiggestNumberFinder.py
+++ BiggestNumberFinder.py
@@ -3,11 +3,15 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 num3 = float(input("Enter the 3rd number: "))
 # Creating if functions
-if num2 <= num1 and num3 <= num1:
+if num2 < num1 and num3 < num1:
     print ("The Biggest Number is: Num 1")
-elif num1 <= num2 and num3 <= num2:
+elif num1 < num2 and num3 < num2:
     print ("The Biggest Number is: Num 2")
+elif num1 == num2 == num3:
+    print ("All Numbers are Equal")
 else:
     print ("The Biggest Number is: Num 3")
 
 # Design for wow factor
+
+
